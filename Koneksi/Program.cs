de1f3393f2680657259fb208c200c38c96a01fe8
MuhammadIhsan45 <muhsanz41@gmail.com>
2022-03-24 04:16:24 +0700

    menambahkan data

--- Koneksi/Program.cs
+++ Koneksi/Program.cs
@@ -9,21 +9,37 @@ namespace Koneksi
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            new Program().Connecting();
+            new Program().InsertData();
         }
-
-        public void Connecting()
+        public void InsertData()
         {
-            using (
-                SqlConnection con = new SqlConnection("data source=IHSAN\\MUHAMMADIHSAN;database=IHSAN_EXE12;User ID=sa;Password=123")
-            )
+            SqlConnection con = null;
+            try
             {
+                con = new SqlConnection("data source = IHSAN\\MUHAMMADIHSAN; database = IHSAN_EXE12; User ID = sa; Password = 123");
                 con.Open();
-                Console.WriteLine("Berhasil terhubung ke database!");
+
+                SqlCommand cm = new SqlCommand("INSERT INTO Client (ClientId, ClientNo, Nama, Umur, Pekerjaan, Alamat, Telp) VALUES ('ID01','CO1','Agus','30','Dokter','bantul','813924982')" +
+                    "INSERT INTO Client (ClientId, ClientNo, Nama, Umur, Pekerjaan, Alamat, Telp) VALUES ('ID02','CO2','Bambang','31','Dosen','bantul','813924932')" + "INSERT INTO Client " +
+                    "(ClientId, ClientNo, Nama, Umur, Pekerjaan, Alamat, Telp) VALUES ('ID03','CO3','Rohmat','32','Pilot','bantul','813925983')" + "INSERT INTO Client (ClientId, ClientNo, Nama," +
+                    " Umur, Pekerjaan, Alamat, Telp) VALUES ('ID04','CO4','Firaun','33','Guru','bantul','813924934')" + "INSERT INTO Client (ClientId, ClientNo, Nama, Umur, Pekerjaan, Alamat, Telp) VALUES ('ID05','CO5','Ahmad','34','Supir','bantul','813925953')", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Sukses menambahkan data");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Gagal menambahkan data, " + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
             }
         }
-
     }
 }
